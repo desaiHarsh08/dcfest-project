fa37665c14e18fc54f02941d327398dcd2b8d3f2
desaiHarsh08 <desaiharsh183@gmail.com>
2025-12-19 19:37:04 +0530

    removed count

--- frontend/dc-fest/src/pages/EventParticipationPage.jsx
+++ frontend/dc-fest/src/pages/EventParticipationPage.jsx
@@ -617,7 +617,7 @@ const EventParticipationPage = () => {
               const participantCount = participants.filter((p) => p.collegeId == college.id).length;
               return (
                 <option key={`college-${collegeIndex}`} value={college.id}>
-                  {college.icCode} - {college?.name} | {participantCount > 0 ? `(${participantCount})` : "(0)"}
+                  {college.icCode} - {college?.name}
                 </option>
               );
             })}
